95d9580cb96ff4444d97468c8817e32a00b0ce62
LEAKONO <leakonoemmanuel3@gmail.com>
2025-03-29 16:54:33 +0300

    modified frontend

--- frontend/medical-dashboard/src/api/axios.js
+++ frontend/medical-dashboard/src/api/axios.js
@@ -5,7 +5,6 @@ const api = axios.create({
   baseURL: 'https://doctors-appointment-5egi.onrender.com/api', 
 });
 
-// Request interceptor
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
